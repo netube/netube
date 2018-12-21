9e539d45d922c7dba608cb47a87c54dbfe39b9a3
Bing Djeung <bing@djeung.org>
2018-12-21 22:16:47 +0800

    Update echo server

--- Sources/netube/EchoServer.swift
+++ Sources/netube/EchoServer.swift
@@ -3,63 +3,50 @@ import Socket
 import Dispatch
 
 class EchoServer {
-        
-        static let quitCommand: String = "QUIT"
-        static let shutdownCommand: String = "SHUTDOWN"
-        static let bufferSize: Int = 4096
-        
-        let port: Int
-        var listenSocket: Socket? = nil
-        var continueRunning: Bool = true
-        var connectedSockets: [Int32: Socket] = [Int32: Socket]()
-        let socketLockQueue: DispatchQueue = DispatchQueue(label: "org.netube.EchoServer.socketLockQueue")
+        private static let quitCommand: String = ":quit"
+        private static let shutdownCommand: String = ":off"
+        private static let bufferSize: Int = 4096
+
+        private let port: Int
+        private var listenSocket: Socket? = nil
+        private var isRunning: Bool = true
+        private var connectedSockets: [Int32: Socket] = [Int32: Socket]()
+        private let socketLockQueue: DispatchQueue = DispatchQueue(label: "org.netube.EchoServer.socketLockQueue")
         
         init(port: Int) {
                 self.port = port
         }
-        
         deinit {
-                // Close all open sockets...
                 connectedSockets.values.forEach { $0.close() }
-                
-                self.listenSocket?.close()
+                listenSocket?.close()
         }
         
         func run() {
-                
                 let queue = DispatchQueue.global(qos: .userInteractive)
-                
                 queue.async { [unowned self] in
-                        
                         do {
-                                // Create an IPV6 socket...
                                 try self.listenSocket = Socket.create(family: .inet6)
-                                
                                 guard let socket: Socket = self.listenSocket else {
                                         print("Unable to unwrap socket...")
                                         return
                                 }
-                                
                                 try socket.listen(on: self.port)
-                                
                                 print("Listening on port: \(socket.listeningPort)")
-                                
+
                                 repeat {
                                         let newSocket: Socket = try socket.acceptClientConnection()
-                                        
                                         print("Accepted connection from: \(newSocket.remoteHostname) on port \(newSocket.remotePort)")
                                         print("Socket Signature: \(String(describing: newSocket.signature?.description))")
-                                        
+
                                         self.addNewConnection(socket: newSocket)
-                                        
-                                } while self.continueRunning
-                                
+                                } while self.isRunning
+
                         } catch let error {
                                 guard let socketError: Socket.Error = error as? Socket.Error else {
                                         print("Unexpected error...")
                                         return
                                 }
-                                if self.continueRunning {
+                                if self.isRunning {
                                         print("Error reported:\n \(socketError.description)")
                                 }
                         }
@@ -67,69 +54,45 @@ class EchoServer {
                 dispatchMain()
         }
         
-        func addNewConnection(socket: Socket) {
+        private func addNewConnection(socket: Socket) {
                 
                 // Add the new socket to the list of connected sockets...
                 socketLockQueue.sync { [unowned self, socket] in
                         self.connectedSockets[socket.socketfd] = socket
                 }
                 
-                // Get the global concurrent queue...
-                let queue = DispatchQueue.global(qos: .default)
+                let queue: DispatchQueue = DispatchQueue.global(qos: .default)
                 
                 // Create the run loop work item and dispatch to the default priority global queue...
                 queue.async { [unowned self, socket] in
                         
-                        var shouldKeepRunning = true
+                        var isConnected: Bool = true
                         
-                        var readData = Data(capacity: EchoServer.bufferSize)
+                        var readData: Data = Data(capacity: EchoServer.bufferSize)
                         
                         do {
-                                // Write the welcome string...
-                                try socket.write(from: "Hello, type 'QUIT' to end session\nor 'SHUTDOWN' to stop server.\n")
-                                
+                                try socket.write(from: "Hello, type '\(EchoServer.quitCommand)' to end session" +
+                                        "or '\(EchoServer.shutdownCommand)' to stop server.\n")
                                 repeat {
                                         let bytesRead = try socket.read(into: &readData)
-                                        
                                         if bytesRead > 0 {
                                                 guard let response = String(data: readData, encoding: .utf8) else {
                                                         print("Error decoding response...")
                                                         readData.count = 0
                                                         break
                                                 }
-                                                if response.hasPrefix(EchoServer.shutdownCommand) {
-                                                        print("Shutdown requested by connection at \(socket.remoteHostname):\(socket.remotePort)")
-                                                        
-                                                        // Shut things down...
-                                                        self.shutdownServer()
-                                                        return
-                                                }
-                                                print("Server received from connection at \(socket.remoteHostname):\(socket.remotePort): \(response) ")
-                                                let reply = "Server response: \n\(response)\n"
-                                                try socket.write(from: reply)
-                                                
-                                                if (response.uppercased().hasPrefix(EchoServer.quitCommand) || response.uppercased().hasPrefix(EchoServer.shutdownCommand)) &&
-                                                        (!response.hasPrefix(EchoServer.quitCommand) && !response.hasPrefix(EchoServer.shutdownCommand)) {
-                                                        try socket.write(from: "If you want to QUIT or SHUTDOWN, please type the name in all caps. 😃\n")
-                                                }
-                                                
-                                                if response.hasPrefix(EchoServer.quitCommand) || response.hasSuffix(EchoServer.quitCommand) {
-                                                        shouldKeepRunning = false
-                                                }
+                                                try self.dealingResponse(socket: socket, response: response, keep: &isConnected)
                                         }
-                                        
                                         if bytesRead == 0 {
-                                                shouldKeepRunning = false
+                                                isConnected = false
                                                 break
                                         }
-                                        
                                         readData.count = 0
                                         
-                                } while shouldKeepRunning
+                                } while isConnected
                                 
                                 print("Socket: \(socket.remoteHostname):\(socket.remotePort) closed...")
                                 socket.close()
-                                
                                 self.socketLockQueue.sync { [unowned self, socket] in
                                         self.connectedSockets[socket.socketfd] = nil
                                 }
@@ -139,22 +102,32 @@ class EchoServer {
                                         print("Unexpected error by connection at \(socket.remoteHostname):\(socket.remotePort)...")
                                         return
                                 }
-                                if self.continueRunning {
+                                if self.isRunning {
                                         print("Error reported by connection at \(socket.remoteHostname):\(socket.remotePort):\n \(socketError.description)")
                                 }
                         }
                 }
         }
-        
-        func shutdownServer() {
+        private func dealingResponse(socket: Socket, response: String, keep: inout Bool) throws {
+                if response.lowercased.hasPrefix(EchoServer.shutdownCommand) {
+                        print("Shutdown requested by connection at \(socket.remoteHostname):\(socket.remotePort)")
+                        self.shutdownServer()
+                        return
+                }
+                print("Server received from connection at \(socket.remoteHostname):\(socket.remotePort): \(response) ")
+                let reply: String = "Server response: \n\(response)\n"
+                try socket.write(from: reply)
+
+                if response.lowercased.hasPrefix(EchoServer.quitCommand) || response.hasSuffix(EchoServer.quitCommand) {
+                        keep = false
+                }
+        }
+
+        private func shutdownServer() {
                 print("\nShutdown in progress...")
-                continueRunning = false
-                
-                // Close all open sockets...
+                isRunning = false
                 connectedSockets.values.forEach { $0.close() }
-                
                 listenSocket?.close()
-                
                 DispatchQueue.main.sync {
                         exit(0)
                 }
